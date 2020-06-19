a9f8c3c346d0eb2f1640bc708d01c70d3b901d1b
viktor_kalanda <viktor.kalanda@diaceutics.com>
2020-06-19 15:29:51 +0300

    add lombook for Platform

--- diaceutics-selenium/src/main/java/diaceutics/selenium/models/Platform.java
+++ diaceutics-selenium/src/main/java/diaceutics/selenium/models/Platform.java
@@ -1,23 +1,11 @@
 package diaceutics.selenium.models;
 
+import lombok.Data;
+
+@Data
 public class Platform extends BaseModel{
 
     private String platformManufacturer;
     private String platform;
 
-    public String getPlatformManufacturer() {
-        return platformManufacturer;
-    }
-
-    public String getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(String platform) {
-        this.platform = platform;
-    }
-
-    public void setPlatformManufacturer(String platformManufacturer) {
-        this.platformManufacturer = platformManufacturer;
-    }
 }
